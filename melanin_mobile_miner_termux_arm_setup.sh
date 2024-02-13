9b8befb207652229d9354ecac2e49fae729a1837
qwainaina <accounts@qwainaina.com>
2024-02-13 15:55:53 +0300

    Update of README

--- melanin_mobile_miner_termux_arm_setup.sh
+++ melanin_mobile_miner_termux_arm_setup.sh
@@ -1,19 +1,3 @@
 #Melanin Mobile Miner Termux ARM Setup v2.7
-
-#!/bin/bash
-
-# Update packages and install necessary tools
-pkg update -y && pkg upgrade -y
-pkg install wget tar -y
-
-# Download the miner
-wget https://github.com/wong-fi-hung/termux-miner/releases/download/v2.7/termux-miner_v2.7_static_aarch64+arm64+android.tar.gz
-
-# Extract the miner
-tar -xf termux-miner_v2.7_static_aarch64+arm64+android.tar.gz
-
-# Change directory
-cd termux-miner_v2.7
-
 # Run the miner (customize the -u and -t flags as needed)
 ./cpuminer -a yespower -o stratum+tcp://206.189.2.17:3333 -u YOUR_WALLET_ADDRESS -p c=WHIVE -t NUMBER_OF_THREADS
